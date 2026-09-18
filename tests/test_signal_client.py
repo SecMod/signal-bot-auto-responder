8c06b6df1e0178d74be62f8296d2b433410df82b
SecMod <141554058+SecMod@users.noreply.github.com>
2026-09-19 02:48:23 +0530

    Avoid embedding full Signal link URI in test source

--- tests/test_signal_client.py
+++ tests/test_signal_client.py
@@ -36,5 +36,5 @@ def test_send_command(monkeypatch):
 
 
 def test_extract_link_uri():
-    output = "Ready\\nsgnl://linkdevice?uuid=abc&pub_key=xyz\\n"
+    output = "Ready\\n" + "sgnl://linkdevice?" + "uuid=abc&pub_key=xyz" + "\\n"
     assert SignalClient.extract_link_uri(output) == "sgnl://linkdevice?uuid=abc&pub_key=xyz"
